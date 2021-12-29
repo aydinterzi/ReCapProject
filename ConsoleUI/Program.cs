5ce8b73c0bf24c99c387776d7eb48c802e01a318
Aydın Terzi <aydinterzi7@gmail.com>
2021-12-29 15:04:24 +0300

    ientityrepository & ef added

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,9 +1,11 @@
 ﻿
-
+using Entities.Concrete;
 using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
 using DataAccess.Concrete.InMemory;
+CarManager carManager = new CarManager(new EfCarDal());
+carManager.Add(new Car {Id=5,DailyPrice=0,BrandId=3,ColorId=2,Description="güzel araba",ModelYear=2021 });
 
-CarManager carManager = new CarManager(new InMemoryCarDal());
 foreach (var item in carManager.GetAll())
 {
     Console.WriteLine(item.Description);
---
+++ DataAccess/Concrete/EntityFramework/ReCapContext.cs
@@ -0,0 +1,21 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class ReCapContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=DESKTOP-TO5DICM\SQLEXPRESS;Database=ReCap;Trusted_Connection=true");
+        }
+        public DbSet<Brand> Brands { get; set; }
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<Color> Colors { get; set; }
+    }
+}
