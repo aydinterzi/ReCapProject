884c3485b85813ffb45a529351019fa06caf5c9d
Aydın Terzi <aydinterzi7@gmail.com>
2021-12-31 12:06:26 +0300

    user-customer-rental tables added

---
+++ Business/Concrete/RentalManager.cs
@@ -0,0 +1,43 @@
+﻿using Business.Abstract;
+using Core.Results;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class RentalManager:IRentalService
+    {
+        IRentalDal _rentalDal;
+        public RentalManager(IRentalDal rentalDal)
+        {
+            _rentalDal = rentalDal;
+        }
+        public IResult Add(Rental rental)
+        {
+            if (rental.ReturnDate == null)
+            {
+                _rentalDal.Add(rental);
+                return new SuccessResult();
+            }
+            return new ErrorResult();
+            
+        }
+
+        public IResult Delete(Rental rental)
+        {
+            _rentalDal.Delete(rental);
+            return new SuccessResult();
+        }
+
+        public IResult Update(Rental rental)
+        {
+            _rentalDal.Update(rental);
+            return new SuccessResult();
+        }
+    }
+}
